172e30d2e0f5fa04bca52b36bd2d52c4a1a0c42e
学习C语言的第一天 <136468348+newcakes@users.noreply.github.com>
2023-07-28 09:57:19 +0800

    本地文件存储通讯录

---
+++ 第四十六天/contact.h
@@ -0,0 +1,48 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<errno.h>
+#include<Windows.h>
+//#define MAX 1000
+#define INIT 3
+#define ADD 2
+#define MAX_NAME 20
+#define MAX_SEX 20
+#define MAX_TELE 15
+#define MAX_ADDR 25
+enum option
+{
+	Exit,
+	add,
+	del,
+	search,
+	modify,
+	show,
+	sort,
+	save
+};
+struct ContactInfo
+{
+	char name[MAX_NAME];
+	int age;
+	char sex[MAX_SEX];
+	char tele[MAX_TELE];
+	char addr[MAX_ADDR];
+};
+struct Contact
+{
+	int size;
+	int capacity;
+	struct ContactInfo *date;
+};
+void InitContact(struct Contact* ps);
+void AddContact(struct Contact* ps);
+void ShowContact(struct Contact* ps);
+void DelContact(struct Contact* ps);
+void SearchContact(struct Contact* ps);
+void ModifyContact(struct Contact* ps);
+void SortContact(struct Contact* ps);
+void DextroyContact(struct Contact* ps);
+void SaveContact(struct Contact* ps);
+void LandContact(struct Contact* ps);
---
+++ 第四十六天/text.c
@@ -0,0 +1,67 @@
+#include"contact.h"
+void Pause()
+{
+	printf("三秒后返回首页！\n");
+	Sleep(3000);
+	system("cls");
+}
+void menu()
+{
+	printf("**************************\n");
+	printf("***1.add      2.del    ***\n");
+	printf("***3.search   4.modify ***\n");
+	printf("***5.show     6.sort  ***\n");
+	printf("***0.exit              ***\n");
+	printf("**************************\n");
+}
+int main()
+{
+	int  input = 0;	
+	struct Contact con = {0};
+	InitContact(&con);	
+	do
+	{	
+		menu();
+		printf("请选择——> ");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case add:
+			AddContact(&con);
+			Pause();
+			break;
+		case del:			
+			DelContact(&con);
+			Pause();
+			break;
+		case search:
+			SearchContact(&con);
+			Pause();
+			break;
+		case modify:
+			ModifyContact(&con);
+			Pause();
+			break;
+		case show:
+			ShowContact(&con);
+			break;
+		case sort:
+			SortContact(&con);
+			Pause();
+			break;
+		case save:
+			SaveContact(&con);
+			Pause();
+			break;
+		case Exit:
+			SaveContact(&con);
+			DextroyContact(&con);
+			printf("退出成功！");
+			break;
+		default:
+			printf("输入有误，请重新输入！");
+			break;
+		}				
+	} while (input);
+	return 0;
+}
